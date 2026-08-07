83f2a1eb59d68c270568b4b9365373254867953b
matheuss0201 <matheuss0201@gmail.com>
2026-08-06 21:28:24 -0300

    docs: adicionar arquivo README.md com a documentação do projeto

---
+++ skillmatch.js
@@ -0,0 +1,68 @@
+
+class Vaga {
+    constructor(id, titulo, requisitos) {
+        this.id = id;
+        this.titulo = titulo;
+        this.requisitos = requisitos; 
+    }
+}
+
+class VagaFrontEnd extends Vaga {
+    constructor(id, titulo, requisitos, nivel = "Júnior") {
+        super(id, titulo, requisitos);
+        this.nivel = nivel; 
+    }
+}
+
+
+const vagasDisponiveis = [
+    new VagaFrontEnd(1, "Vaga A (Foco em React)", ["HTML", "CSS", "JavaScript", "React", "Git"]),
+    new VagaFrontEnd(2, "Vaga B (Foco em Design/UI)", ["HTML", "CSS", "JavaScript", "Tailwind", "Figma"]),
+    new VagaFrontEnd(3, "Vaga C (Foco em Backend)", ["HTML", "CSS", "JavaScript", "Node.js", "TypeScript"])
+];
+
+const listaCandidatos = [
+    {
+        nome: "Lucas Silva",
+        habilidades: ["HTML", "CSS", "JavaScript", "Git"],
+        estaAtivo: true,
+        anosExperiencia: 0
+    },
+    {
+        nome: "Ana Costa",
+        habilidades: ["HTML", "CSS", "JavaScript", "Tailwind", "React"],
+        estaAtivo: true,
+        anosExperiencia: 1
+    }
+];
+
+
+//  USO DE CLOSURE E CALLBACK
+function criarContadorAnalises() {
+    let contagem = 0;
+    return function() {
+        contagem += 1; 
+        return contagem;
+    };
+}
+const incrementarContador = criarContadorAnalises();
+
+function formatarMensagem(texto, callback) {
+    return callback(texto);
+}
+const callbackCaixaAlta = (txt) => txt.toUpperCase();
+
+
+//  PROMISE E ASYNC/AWAIT
+
+function buscarDadosVagasPro() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (vagasDisponiveis.length > 0) {
+                resolve(vagasDisponiveis);
+            } else {
+                reject("Nenhuma vaga encontrada no sistema.");
+            }
+        }, 1000);
+    });
+}
